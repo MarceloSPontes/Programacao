fc6924423b42b2152c8b656b63c24278fa24262e
Marcelo Pontes <108842039+MarceloSPontes@users.noreply.github.com>
2023-02-03 11:41:32 -0300

    Create App.js

---
+++ FAP/REACT/Exercicios/Exercício 05/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Hooks from './components/Hooks';
+
+function App() {
+const nome = 'Maria'
+
+  return (
+    <div className="App">
+      <h1>Projeto de Marcelo Pontes</h1>    
+      <Hooks/>
+    </div>
+  );
+}
+
+export default App;
+
